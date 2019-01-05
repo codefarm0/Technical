1c150dc08476fd2dfb5e9af4384ad466445c175c
greenlearner01 <greenlearner01@users.noreply.github.com>
2019-01-05 12:22:14 +0530

    Create OneDimensionalArrayDemo.java
    First array programme to print the elements

---
+++ DataStructure/src/com/gl/arrays/OneDimensionalArrayDemo.java
@@ -0,0 +1,14 @@
+package com.gl.arrays;
+
+public class OneDimensionalArrayDemo {
+
+	public static void main(String[] args) {
+		int[] array = new int[]{1,2,3,4};
+		System.out.println("Below are the elements of the array - > \n");
+		for(int i : array) {
+			System.out.print(i + ", ");
+		}
+
+	}
+
+}
