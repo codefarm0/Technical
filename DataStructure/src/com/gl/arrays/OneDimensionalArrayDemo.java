038ccb38cce23887868e6a429a1cbbd7267760af
greenlearner01 <greenlearner01@users.noreply.github.com>
2019-01-08 10:22:31 +0530

    Update OneDimensionalArrayDemo.java
    Making arrays more granular to make things clear

--- DataStructure/src/com/gl/arrays/OneDimensionalArrayDemo.java
+++ DataStructure/src/com/gl/arrays/OneDimensionalArrayDemo.java
@@ -3,12 +3,30 @@ package com.gl.arrays;
 public class OneDimensionalArrayDemo {
 
 	public static void main(String[] args) {
-		int[] array = new int[]{1,2,3,4};
+		int[] array;//declaration
+		array = new int[10];//instantiation 
+		array[0]=10;
+		array[1]=20;
+		array[2]=30;
+		array[3]=30;
+		array[4]=40;
+		array[5]=50;
 		System.out.println("Below are the elements of the array - > \n");
+		printElements(array);
+		
+		//declaration, instantiation and initialization in one line
+		System.out.println("declaration, instantiation and initialization in one line - > \n");
+		int[] array2 = new int[] {1,2,4,5};
+		printElements(array2);
+
+	}
+
+	private static void printElements(int[] array) {
+		
 		for(int i : array) {
 			System.out.print(i + ", ");
 		}
-
+		System.out.println();
 	}
 
 }
