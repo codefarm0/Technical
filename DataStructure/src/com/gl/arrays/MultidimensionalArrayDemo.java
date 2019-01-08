9626c0166467a77e222d5cd182a8e8f988a50c40
greenlearner01 <greenlearner01@users.noreply.github.com>
2019-01-08 11:16:37 +0530

    Finalizing for demo

--- DataStructure/src/com/gl/arrays/MultidimensionalArrayDemo.java
+++ DataStructure/src/com/gl/arrays/MultidimensionalArrayDemo.java
@@ -1,10 +1,15 @@
 package com.gl.arrays;
+/**
+ * 
+ * Arrays are passed by reference
+ *
+ */
 
 public class MultidimensionalArrayDemo {
 
 	public static void main(String[] args) {
 		showTwoDimensionalArray();
-		showThreeDimensionalArray();
+		//showThreeDimensionalArray();
 
 	}
 
@@ -12,16 +17,88 @@ public class MultidimensionalArrayDemo {
 	 * This method explains about 2-D array
 	 */
 	private static void showTwoDimensionalArray() {
-		// TODO Auto-generated method stub
+		
+		String[][] twoDimeArray2 = new String[3][3];//array declaration as well as memory allocation
+		
+		twoDimeArray2[0][0] = "Fruits";
+		twoDimeArray2[0][1] = "Flowers";
+		twoDimeArray2[0][2] = "Vegetables";
+		
+		printELements(twoDimeArray2);
+		
+		String[][] twoDimeArray = new String[][]{
+				{"Fruits","mango","apple","guava"},
+				{"Flowers","rose","lotus","kevada"},
+				{"Vegetables","cabage","peas","carrot"}
+		};
+		
+		printElementsOf2Darray(twoDimeArray);
+		
+		
 		
 	}
 	
+	private static void printELements(String[][] twoDimeArray2) {
+		for(int i= 0; i<3; i++) {
+			for(int j=0;j<3;j++) {
+				System.out.println(twoDimeArray2[i][j] + ", ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+	private static void printElementsOf2Darray(String[][] twoDimeArray) {
+		System.out.println("Length of the array -> " + twoDimeArray.length);
+		for(String[] elements : twoDimeArray) {
+			for(String element : elements) {
+				System.out.print(element + ", ");
+			}
+			System.out.println();
+		}
+		
+	}
+
 	/**
 	 * This method explains about 3-D array
 	 */
 	private static void showThreeDimensionalArray() {
-		// TODO Auto-generated method stub
+		String[][][] threeDimArray1 = new String[3][3][3];//array declaration as well as memory allocation
 		
+		threeDimArray1[0][0][0] = "Fruits";
+		threeDimArray1[0][0][1] = "Flowers";
+		threeDimArray1[0][0][2] = "Vegetables";
+		
+		String[][][] threeDimArray2 = new String[][][]{
+			{
+				{"Fruits","mango","apple","guava"},
+				{"Flowers","rose","lotus","kevada"},
+				{"Vegetables","cabage","peas","carrot"}
+			},
+			{
+				{"profession", "government", "non-govenrment"},
+				{"country", "India", "USA"}
+			},
+			{
+				{"states", "UP", "Delhi"},
+				{"cities", "Noida", "Pune"}
+			}
+		};
+		
+		printElementsOf3Darray(threeDimArray2);
+		
+	}
+
+	private static void printElementsOf3Darray(String[][][] threeDimArray2) {
+		System.out.println("Length of the array -> " + threeDimArray2.length);
+		for(String[][] twoDArray : threeDimArray2) {
+		for(String[] oneDarray : twoDArray) {
+			for(String element : oneDarray) {
+				System.out.print(element + ", ");
+			}
+			System.out.println();
+		}
+		}
 	}
 
 }
--- DataStructure/src/com/gl/arrays/OperationsOnArray.java
+++ DataStructure/src/com/gl/arrays/OperationsOnArray.java
@@ -1,10 +1,90 @@
 package com.gl.arrays;
 
+/**
+ * 
+ * @author Arvind
+ * 
+ * Arrays are passed by reference
+ *
+ */
 public class OperationsOnArray {
 
 	public static void main(String[] args) {
+		int array[] = new int [] {1,2,3,4,5};
 		
+		System.out.println("Print initial array -->");
+		
+		printArrayELements(array);
+		
+		System.out.println("------------------removing elements from array---------------------");
+		removeElementFromArray(array, 2);
+		System.out.println("After removing particular element, elements of the array -->");
+		printArrayELements(array);
+		
+		System.out.println("------------------Adding elements into array---------------------");
+		addELementIntoArray(array, 200);
+		System.out.println("After adding particular element, elements of the array -->");
+		printArrayELements(array);
 
+	}
+
+	/**
+	 * Adds element into the array
+	 * @param array
+	 * @param elementToAdd
+	 */
+	private static void addELementIntoArray(int[] array, int elementToAdd) {
+		int length = array.length;
+		boolean flag = false;
+		for(int i = 0; i< length; i++) {
+			if(array[i]==0) {
+				array[i]=elementToAdd;
+				flag=true;
+				System.out.println("Element added at index - " + i + " \n");
+				break;
+			}
+		}
+		
+		if(!flag) {
+			System.out.println("There is no more space to add elements\n");
+		}
+		
+	}
+
+	/**
+	 * If the specified element is present in the array then removes it from the array
+	 * @param array
+	 * @param elementToRemove
+	 */
+	private static void removeElementFromArray(int[] array, int elementToRemove) {
+		
+		int length = array.length;
+		boolean flag = false;
+		for(int i = 0; i< length; i++) {
+			if(array[i]==elementToRemove) {
+				array[i]=0;
+				flag=true;
+				System.out.println("Element found at index - " + i + " and removed from array \n");
+				break;
+			}
+		}
+		
+		if(!flag) {
+			System.out.println("ELement to remove is not present in the array\n");
+		}
+		
+	}
+
+	/**
+	 * Prints the elements of array
+	 * @param array
+	 */
+	private static void printArrayELements(int[] array) {
+		for(int element : array) {
+			System.out.print(element + ", ");
+		}
+		System.out.println("");
+		
 	}
 
 }
