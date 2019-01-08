b79258664e5968a41c0d558485f544e1850673a0
greenlearner01 <greenlearner01@users.noreply.github.com>
2019-01-08 10:23:29 +0530

    More demo classes on array
    More demo classes on array

---
+++ DataStructure/src/com/gl/arrays/MultidimensionalArrayDemo.java
@@ -0,0 +1,27 @@
+package com.gl.arrays;
+
+public class MultidimensionalArrayDemo {
+
+	public static void main(String[] args) {
+		showTwoDimensionalArray();
+		showThreeDimensionalArray();
+
+	}
+
+	/**
+	 * This method explains about 2-D array
+	 */
+	private static void showTwoDimensionalArray() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 * This method explains about 3-D array
+	 */
+	private static void showThreeDimensionalArray() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ DataStructure/src/com/gl/arrays/OperationsOnArray.java
@@ -0,0 +1,10 @@
+package com.gl.arrays;
+
+public class OperationsOnArray {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
